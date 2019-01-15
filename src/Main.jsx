7002f12d85873ac716bc2bc0ad254ce389d7448a
Sean Stephenson <Sean.M.Stephenson@espn.com>
2019-01-15 13:02:23 -0800

    mucking with classes

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,27 +1,33 @@
 import React from 'react'
-import { Alert } from 'reactstrap'
+import {
+  Alert, Row, Col,
+} from 'reactstrap'
 import { useGlobal, setGlobal } from 'reactn'
 
 const Main = () => {
   const [alert] = useGlobal('alert')
   const [highlight] = useGlobal('highlight')
   return (
-    <div id="alert">
-      <Alert color="secondary">
-        {(typeof alert === 'string' && alert)}
-        { (alert || (highlight && highlight.length > 0)) && (
-          <button
-            onClick={
+    <div id="main">
+      <Row>
+        <Col sm="12">
+          <Alert color="secondary" id="main-alert">
+            {(typeof alert === 'string' && alert)}
+            { (alert || (highlight && highlight.length > 0)) && (
+            <button
+              onClick={
               () => { setGlobal({ alert: null, highlight: [] }) }
             }
-            type="button"
-            className="close"
-            aria-label="clear"
-          >
-            <span aria-hidden="true">&times;</span>
-          </button>
-        )}
-      </Alert>
+              type="button"
+              className="close"
+              aria-label="clear"
+            >
+              <span aria-hidden="true">&times;</span>
+            </button>
+            )}
+          </Alert>
+        </Col>
+      </Row>
     </div>
   )
 }
